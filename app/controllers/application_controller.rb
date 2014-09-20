fe13c37ad5b055d2fbe05a91c941ae93ee5aeb7f
Evan Derby <somekidwithhtml@gmail.com>
2014-09-19 22:14:17 -0600

    Fixed navbar & font styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
 	before_action :fetch_pages	
 
 	def fetch_pages
-		@pages = Page.all
-	end
+		@pages = Page.order(:created_at)
+  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
